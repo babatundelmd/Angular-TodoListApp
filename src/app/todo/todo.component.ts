90e79a1ff2ba363f0329c84d9fa8dbd9721f34cb
Babatunde Lamidi <odunayohlamidi@gmail.com>
2019-12-18 15:50:19 +0100

    ✅chore: onAdd function works!

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,26 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { element } from '@angular/core/src/render3';
 
-@Component( {
+@Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: [ './todo.component.css' ]
-} )
+})
 export class TodoComponent implements OnInit {
   todoListArray: any[];
 
-  constructor ( private todoService: TodoService ) { }
+  constructor (private todoService: TodoService) { }
 
   ngOnInit () {
     this.todoService.getTodoList().snapshotChanges()
-      .subscribe( item => {
+      .subscribe(item => {
         this.todoListArray = [];
-        item.forEach( element => {
+        item.forEach(element => {
           const x = element.payload.toJSON();
           x[ "$key" ] = element.key;
-          this.todoListArray.push( x )
-        } )
-      } )
+          this.todoListArray.push(x)
+        });
+        // sort array isChecked false -> true
+        this.todoListArray.sort((a, b) => a.isChecked - b.isChecked);
+      });
   }
 
+  onAdd (itemTitle) {
+    this.todoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
 }
